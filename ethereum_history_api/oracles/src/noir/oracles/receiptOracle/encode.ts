d3559da36842acf272dfa90931a9b98bb1b4c71f
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-25 12:15:10 +0100

    Implement tabulateStructField & statusToField

--- ethereum_history_api/oracles/src/noir/oracles/receiptOracle/encode.ts
+++ ethereum_history_api/oracles/src/noir/oracles/receiptOracle/encode.ts
@@ -5,13 +5,13 @@ import { padArray } from '../../../util/array.js';
 import { encodeField, encodeHex, encodeProof, encodeBytes } from '../common/encode.js';
 import { ZERO_PAD_VALUE } from '../common/const.js';
 import { ReceiptProof } from '../../../ethereum/receiptProof.js';
+import { BYTES_32_ZERO } from '../../../util/const.js';
 
 const MAX_RECEIPT_PROOF_LEVELS = 7;
 const MAX_RECEIPT_LENGTH = 532;
 export const MAX_RECEIPT_RLP_LENGTH = 532;
 const MAX_RECEIPT_PROOF_LENGTH = MAX_RECEIPT_LENGTH * MAX_RECEIPT_PROOF_LEVELS;
 export const KEY_LENGTH = 4;
-const BYTES_32_ZERO = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 export function encodeReceipt(receipt: TransactionReceipt): ForeignCallOutput[] {
   const status = statusToHex(receipt.status);
